2b055f433deafa4eeb9d170c33f3e612cbe75527
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-23 00:11:19 +0600

    Dispatching Correct Values

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import PostList from "./PostList";
+import React from 'react';
+import PostList from './PostList';
 
 const App = () => {
   return (
     <div className="ui container">
-      <PostList></PostList>
+      <PostList />
     </div>
   );
 };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPost } from "../actions";
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
   componentDidMount() {
-    this.props.fetchPost();
+    this.props.fetchPosts();
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
-export default connect(null, { fetchPost })(PostList);
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
